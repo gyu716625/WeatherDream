1dedb1e822e5b02ed63d7e04834ff7236b2a58c3
효진 <hyojin@hyojin-ui-MacBookPro.local>
2020-07-02 16:52:13 +0900

    weather api 불러오기 전

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import App from './component/App';
+import { WEATHER_API_KEY } from "../config/openWeather";
+ 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <App WEATHER_API_KEY={WEATHER_API_KEY} />,
+  document.getElementById("root")
 );
 
