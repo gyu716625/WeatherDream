8cc2eb1c1d5073587927a3d07f833baa556febe5
이나린 <nrecode@NR-MBP.local>
2020-07-02 16:53:42 +0900

    feat: db의 schema를 구현
     - server/config/config.js 파일에 데이터베이스 명을 수정하였습니다.
     - server/schema.sql 파일을 생성하고 db의 스키마를 구현하였습니다.

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ modules.exports={
     development: {
         username: 'root',
         password: process.env.DATABASE_PASSWORD,
-        database: 'shortly',
+        database: 'weatherdream',
         host: 'localhost',
         port: 3306,
         dialect: 'mysql',
