4dcbde4190b53f47be48c52062d436ea8b0185f0
이나린 <nrecode@NR-MBP.local>
2020-07-03 18:53:26 +0900

    Feat: 회원가입 기능을 구현하였습니다.
     - req.body로 들어온 값에 대하여 회원가입을 구현하였습니다.
     - findOrCreate를 이용하여 이미 존재하는 email일시 상태코드 409를 전송합니다.
     - findOrCreate를 이용하여 존재하지 않는 email일시 데이터베이스에 저장하고 상태코드200를 전송합니다.

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,8 +1,27 @@
+const { users } = require('../../models');
+
 module.exports = {
   post: (req, res) => {
-    const result = req.body;
-    console.log(result);
-    res.status(200).json({ message: 'ok!' });
+    const { email, password, username } = req.body;
+    console.log(email);
+    users
+      .findOrCreate({
+        where: {
+          email: email,
+        },
+        defaults: {
+          username: username,
+          password: password,
+        },
+      })
+      .then(async ([user, created]) => {
+        if (!created) {
+          return res.status(409).json({ message: 'email exist' });
+        }
+        const data = await user.get({ plain: true });
+        console.log(data);
+        res.status(200).json({ message: 'successful' });
+      });
   },
 
 };
