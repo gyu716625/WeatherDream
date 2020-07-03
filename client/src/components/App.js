bcdcc0013d23a0ac18a50fd2c5af1ad0753cb518
효진 <hyojin@hyojin-ui-MacBookPro.local>
2020-07-03 14:00:49 +0900

    do not completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
+/*global kakao*/ 
 import React, { Component } from 'react';
 import "./App.css";
 import { weather_API_KEY,  kakaoMap_API_KEY } from "../API_KEY";
 
-// 카카오 지도 api 연동
-// openWeather api 연동 (key 발급)
+// 카카오 지도 api 연동  - 참고링크 : https://velog.io/@bearsjelly/React-kakao-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0-2-%EC%95%B1%ED%82%A4%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0
+// openWeather api 연동 (key 발급) - 연동 완료
 
 class App extends Component {
   constructor(props) {
@@ -11,14 +12,23 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log(document.getElementById("map"));
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
-    };
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoMap_API_KEY}&autoload=false`;
+    document.head.appendChild(script);
 
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    script.onload = () => {
+      kakao.maps.load(() => {
+        let container = document.getElementById("map");
+        let options = {
+          center: new kakao.maps.LatLng(37.506502, 127.053617),
+          level: 7,
+        };
+
+        const map = new window.kakao.maps.Map(container, options);
+      });
+    };
   }
 
   
