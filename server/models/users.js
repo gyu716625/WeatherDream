79f50aadd3f651d9355d0c055f396f097939b9c2
이나린 <nrecode@NR-MBP.local>
2020-07-05 19:06:45 +0900

    Feat: user테이블에 password 암호화 기능을 추가하였습니다.
     - 유저가 회원가입을 진행할 때, 패스워드가 암호화되어 데이터 베이스에 insert 되도록 기능을 추가하였습니다.
     - 유저가 로그인을 진행할 때, 입력한 비밀번호 역시 해싱되어 데이터베이스에 저장되어있는 정보와 비교할 수 있습니다.
     - 암호화에 사용된 모듈은 crypto이며 salt 값은 환경변수에 저장됩니다.

--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const crypto = require('crypto');
+
+const salt = process.env.SALT;
+// console.log('salt',typeof salt)
+
 module.exports = (sequelize, DataTypes) => {
   const users = sequelize.define(
     'users',
@@ -8,7 +13,24 @@ module.exports = (sequelize, DataTypes) => {
       username: DataTypes.STRING,
       password: DataTypes.STRING,
     },
-    {},
+    {
+      hooks: {
+        // 비밀번호를 해싱해서 데이터베이스에 저장한다.
+        beforeCreate: (data) => {
+          const shasum = crypto.createHmac('sha512', salt);
+          shasum.update(data.password);
+          data.password = shasum.digest('hex');
+        },
+        // 데이터를 찾기 전에 들어온 데이터의 password를 해싱함수를 통해 검증할 수 있게 한다.
+        beforeFind: (data) => {
+          if (data.where.password) {
+            const shasum = crypto.createHmac('sha512', salt);
+            shasum.update(data.where.password);
+            data.where.password = shasum.digest('hex');
+          }
+        },
+      },
+    },
   );
 
   // user는 많은 채팅을 가질 수 있다.
