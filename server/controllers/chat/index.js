357401edc727d1b795816a5d504c6d32df27fc60
hyejin <nhj1043@naver.com>
2020-07-07 18:04:56 +0900

    Feat: socket.io로 채팅 접속 테스트 했습니다.
    - chat url endpoint 작성

--- server/controllers/chat/index.js
+++ server/controllers/chat/index.js
@@ -1,6 +1,19 @@
 module.exports = {
   // 소켓 통신 시작
   participation: (req, res) => {
-    res.send('<H1> Hello world </ h1>');
+    const { io } = require('../../app');
+    res.sendFile(__dirname + '/index.html');
+
+    io.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+
+      socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+    });
   },
 };
