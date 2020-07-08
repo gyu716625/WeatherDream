d866c64f89b1cd67a0e081537d7024bf94c73852
효진 <hyojin@hyojin-ui-MacBookPro.local>
2020-07-08 13:46:52 +0900

    feat : mypage ui구현 / 메인페이지 라우터이동
    - feat : locationSearch에서 주소설정 후 ok버튼 클릭 시 날씨 페이지로 이동이 가능
      fetch통해서 서버에서 데이터 가져오는 부분은 아직 적용 전입니다

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,25 +3,35 @@ import React, { Component } from 'react';
 import "./App.css";
 // import { weather_API_KEY,  kakaoMap_API_KEY } from "../API_KEY";
 import LocationSearch from './LocationSearch';
+import Weather from './Weather';
 import Login from "./login.component";
 import SignUp from "./signup.component";
+import MyPage from './MyPage';
+import { Route, Switch } from "react-router-dom";
+
 // 카카오 지도 api 연동  - 참고링크 : https://velog.io/@bearsjelly/React-kakao-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0-2-%EC%95%B1%ED%82%A4%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0
 // openWeather api 연동 (key 발급) - 연동 완료
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+    }
   }
 
 
   render() {
     return (
       <div className="APP">
-        <LocationSearch />
-        <Login/>
-        <SignUp/>
+        <Switch>
+          <Route path="/LocationSearch" component={LocationSearch} />
+          <Route path="/Weather" component={Weather} />
+        </Switch>
+        {/* <Login/> */}
+        {/* <SignUp/> */}
+        {/* <MyPage /> */}
       </div>
-    )
+    );
   }
 }
 
