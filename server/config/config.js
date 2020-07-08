0b03208f18508051cca273e68e1a0eed988e8855
hyejin <nhj1043@naver.com>
2020-07-08 12:00:47 +0900

    Chore: 배포에서 사용한 데이터베이스 환경변수 값을 변경했습니다

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  development: {
+  test: {
     username: 'root',
     password: process.env.DATABASE_PASSWORD,
     database: 'weatherdream',
     host: 'localhost',
     port: 3306,
+    dialect: 'mysql',
+    logging: false,
+  },
+  development: {
+    username: 'root',
+    password: process.env.DATABASE_PASSWORD,
+    database: 'weatherdream',
+    host: 'localhost',
+    port: 13306,
     dialect: 'mysql',
     logging: false,
   },
